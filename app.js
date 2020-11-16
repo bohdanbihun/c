b9c55b4ecf0ebd2d9846fd3dc53484aeb427e79c
bohdan bihun <enigmaautoeinal@gmail.com>
2020-11-16 16:26:51 +0200

    Initial 4 .

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ function calculate(){
  var x=document.getElementById('cathetus1').value;
  var y=document.getElementById('cathetus2').value;
 	
+
+	if(x>0 && y>0  ) {
+	
 var b=Math.sqrt(x*x + y*y);	
 	
 alert("гіпотенуза="+b);
 }
+	else alert("---")
+}
+
+
+
